250aab2459804a269ad19f5ed701f61881017bc7
aleksandarPaytalov <apaytalov89@gmail.com>
2024-02-23 18:55:11 +0200

    symplify the Program.cs

--- HouseRentingSystem/Program.cs
+++ HouseRentingSystem/Program.cs
@@ -2,7 +2,7 @@ namespace HouseRentingSystem
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +15,6 @@ namespace HouseRentingSystem
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -23,7 +22,6 @@ namespace HouseRentingSystem
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -35,12 +33,10 @@ namespace HouseRentingSystem
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapDefaultControllerRoute();
             app.MapRazorPages();
 
-            app.Run();
+            await app.RunAsync();
         }
     }
 }
