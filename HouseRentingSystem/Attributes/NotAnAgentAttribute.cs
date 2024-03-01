ac2f2013d82d294e0dd3bdbc6f5314867eb18468
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-01 21:06:01 +0200

    notAnAgent attribute added

---
+++ HouseRentingSystem/Attributes/NotAnAgentAttribute.cs
@@ -0,0 +1,26 @@
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Services;
+using HouseRentingSystem.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HouseRentingSystem.Attributes
+{
+	public class NotAnAgentAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			IAgentService? agentService = context.HttpContext.RequestServices.GetService<AgentService>();
+
+			if (agentService == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			if (agentService != null && agentService.ExistByIdAsync(context.HttpContext.User.Id()).Result)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+			}
+		}
+	}
+}
