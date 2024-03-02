580256ed9f058c532af0dac434546fb8924e5a6a
aleksandarPaytalov <apaytalov89@gmail.com>
2024-03-02 09:40:04 +0200

    small Error Fixed in the MustBeAgent and NonAnAgent attributes

--- HouseRentingSystem/Attributes/MustBeAgent.cs
+++ HouseRentingSystem/Attributes/MustBeAgent.cs
@@ -7,21 +7,24 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace HouseRentingSystem.Attributes
 {
-	public class MustBeAgent :ActionFilterAttribute
-	{
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			IAgentService? agentService = context.HttpContext.RequestServices.GetService<AgentService>();
+    public class MustBeAgentAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
 
-			if (agentService == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
-			}
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
 
-			if (agentService != null && agentService.ExistByIdAsync(context.HttpContext.User.Id()).Result == false)
-			{
-				context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
-			}
-		}
-	}
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService != null
+                && agentService.ExistByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
+            }
+        }
+    }
 }
