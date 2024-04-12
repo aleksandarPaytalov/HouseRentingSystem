10da51788f679255431f912a48c81c581b2217ff
aleksandarPaytalov <apaytalov89@gmail.com>
2024-04-12 12:47:12 +0300

    !

--- HouseRentingSystem.Infrastructure/Data/SeedDb/UserConfiguration.cs
+++ HouseRentingSystem.Infrastructure/Data/SeedDb/UserConfiguration.cs
@@ -1,11 +1,10 @@
 ﻿using HouseRentingSystem.Infrastructure.Data.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace HouseRentingSystem.Infrastructure.Data.SeedDb
 {
-    internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+	internal class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
     {
         public void Configure(EntityTypeBuilder<ApplicationUser> builder)
         {
